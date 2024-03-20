e771500b32242aa0e17a02190944257e1762023d
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2024-03-20 17:57:51 -0300

    add functional query to upcoming events

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,12 +1,37 @@
-import { CalendarOutlined } from '@ant-design/icons'
-import { Card, List } from 'antd'
-import { Text } from '../text'
-import { useState } from 'react'
-import { UESkeleton } from '..'
+import { CalendarOutlined } from '@ant-design/icons';
+import { Badge, Card, List } from 'antd';
+import { Text } from '../text';
+import { useState } from 'react';
+import { UESkeleton } from '..';
+import { getDate } from '@/utilities/helpers';
+import { useList } from '@refinedev/core';
+import { DASHBOARD_CALENDAR_UPCOMING_EVENTS_QUERY } from '@/graphql/queries';
+import dayjs from 'dayjs';
 
 const UpcomingEvents = () => {
 
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const { data, isLoading: eventsLoading } = useList({
+        resource: 'events',
+        pagination: { pageSize: 5 },
+        sorters: [
+            { 
+                field: 'startDate',
+                order: 'asc',
+            }
+        ],
+        filters: [
+            { 
+                field: 'startDate',
+                operator: 'gte', // not like gta. It means greater than
+                value: dayjs().format('YYYY-MM-DD'),
+            }
+        ],
+        meta: {
+            gqlQuery: DASHBOARD_CALENDAR_UPCOMING_EVENTS_QUERY,
+        },
+    });
+    // alert(JSON.stringify(data))
 
     return (
         <Card
@@ -38,8 +63,25 @@ const UpcomingEvents = () => {
                     renderItem={() => <UESkeleton />}
                 />
             ) : (
-                <List>
-
+                <List
+                    itemLayout="horizontal"
+                    dataSource={data?.data || []}
+                    renderItem={(item) => {
+                        
+                        const renderDate = getDate(item.startDate, item.endDate)
+                        return (
+                            <List.Item>
+                                <List.Item.Meta 
+                                    avatar={<Badge color={item.color} />}
+                                    title={<Text size="xs">{renderDate}</Text>}
+                                    description={<Text ellipsis={{ tooltip: true }} strong>
+                                        {item.description}
+                                    </Text>}
+                                />
+                            </List.Item>
+                        )
+                    }}
+                >
                 </List>
             )}
         </Card>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,18 @@
 import UpcomingEvents from './home/upcoming-events';
 import DealsChart from './home/deals-chart';
 import UESkeleton from './skeleton/upcoming-events';
+import AccHdSkeleton from './skeleton/accordion-header';
+import KbColSkeleton from './skeleton/kanban';
+import ProjCardSkeleton from './skeleton/project-card';
+import LtActSkeleton from './skeleton/latest-activities';
 
-export { UpcomingEvents, DealsChart, UESkeleton };
+export { 
+    UpcomingEvents, 
+    DealsChart, 
+    
+    UESkeleton,
+    AccHdSkeleton,
+    KbColSkeleton,
+    ProjCardSkeleton,
+    LtActSkeleton,
+};
