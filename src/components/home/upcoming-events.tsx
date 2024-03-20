a061b28727d2e8ffae9035a8117674327b9e95e9
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2024-03-20 16:53:37 -0300

    add skeleton to Upcoming Events List

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,32 +1,49 @@
 import { CalendarOutlined } from '@ant-design/icons'
-import { Card } from 'antd'
+import { Card, List } from 'antd'
 import { Text } from '../text'
+import { useState } from 'react'
+import { UESkeleton } from '..'
 
 const UpcomingEvents = () => {
-  return (
-    <Card
-        style={{ height: '100%' }}
-        headStyle={{ padding: '8px 16px'}}
-        bodyStyle={{ padding: '0 1rem' }}
-        title={
-            <div style={{
-                display: 'flex',
-                alignItems: 'center',
-                gap: '8px',
-            }}>
-                <CalendarOutlined />
-                <Text
-                    size="sm"
-                    style={{
-                        marginLeft: ".7rem"
-                    }}
-                >UpcomingEvents</Text>
-            </div>
-        }
-    >
-        content
-    </Card>
-  )
+
+    const [isLoading, setIsLoading] = useState(true);
+
+    return (
+        <Card
+            style={{ height: '100%' }}
+            headStyle={{ padding: '8px 16px'}}
+            bodyStyle={{ padding: '0 1rem' }}
+            title={
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '8px',
+                }}>
+                    <CalendarOutlined />
+                    <Text
+                        size="sm"
+                        style={{
+                            marginLeft: ".7rem"
+                        }}
+                    >UpcomingEvents</Text>
+                </div>
+            }
+        >
+            {isLoading ? (
+                <List
+                    itemLayout="horizontal"
+                    dataSource={Array.from({ length: 5 }).map((_, index) => ({
+                        id: index,
+                    }))}
+                    renderItem={() => <UESkeleton />}
+                />
+            ) : (
+                <List>
+
+                </List>
+            )}
+        </Card>
+    )
 }
 
 export default UpcomingEvents
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 import UpcomingEvents from './home/upcoming-events';
 import DealsChart from './home/deals-chart';
+import UESkeleton from './skeleton/upcoming-events';
 
-export { UpcomingEvents, DealsChart };
+export { UpcomingEvents, DealsChart, UESkeleton };
