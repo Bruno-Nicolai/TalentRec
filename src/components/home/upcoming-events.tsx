33b1249e8a7499b83385761caf9725f9f862d5f7
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2024-03-20 16:26:28 -0300

    set upcoming events intial config

--- src/components/home/upcoming-events.tsx
+++ src/components/home/upcoming-events.tsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import { CalendarOutlined } from '@ant-design/icons'
+import { Card } from 'antd'
+import { Text } from '../text'
 
 const UpcomingEvents = () => {
   return (
-    <div>calendar_UpcomingEvents</div>
+    <Card
+        style={{ height: '100%' }}
+        headStyle={{ padding: '8px 16px'}}
+        bodyStyle={{ padding: '0 1rem' }}
+        title={
+            <div style={{
+                display: 'flex',
+                alignItems: 'center',
+                gap: '8px',
+            }}>
+                <CalendarOutlined />
+                <Text
+                    size="sm"
+                    style={{
+                        marginLeft: ".7rem"
+                    }}
+                >UpcomingEvents</Text>
+            </div>
+        }
+    >
+        content
+    </Card>
   )
 }
 
