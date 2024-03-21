37c6aa6ebabb987f517841a76d1cb798eafafaf8
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2024-03-21 18:51:01 -0300

    initialize chart

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,8 +1,40 @@
+import { DollarOutlined } from '@ant-design/icons'
+import { Card } from 'antd'
 import React from 'react'
+import { Text } from '../text'
+import { Area, AreaConfig } from '@ant-design/plots'
 
 const DealsChart = () => {
+
+  const config: AreaConfig = {
+    data: []
+  }
+
   return (
-    <div>dashboard_DealsChart</div>
+    <Card
+      style={{ height: '100%' }}
+      headStyle={{ padding: '8px 16px'}}
+      bodyStyle={{ padding: '24px 24px 0 24px' }}
+      title={
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: '8px',
+          }}
+        >
+          <DollarOutlined />
+          <Text
+            size="sm"
+            style={{ marginLeft: '.5rem' }}
+          >
+            Deals
+          </Text>
+        </div>
+      }
+    >
+      <Area {...config} height={325} />
+    </Card>
   )
 }
 
