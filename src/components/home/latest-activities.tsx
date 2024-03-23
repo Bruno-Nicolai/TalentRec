24d7e53ede7936e3d3f79d5f0b9d282d1c626f72
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2024-03-23 18:46:07 -0300

    active LA Skeleton

--- src/components/home/latest-activities.tsx
+++ src/components/home/latest-activities.tsx
@@ -1,6 +1,43 @@
+import { UnorderedListOutlined } from "@ant-design/icons"
+import { Card, List } from "antd"
+import { Text } from "../text"
+import LatestActivitiesSkeleton from "../skeleton/latest-activities";
+
 const LatestActivities = () => {
+    const isLoading = true/* false */;
   return (
-    <div>LatestActivities</div>
+    <Card
+        headStyle={{ padding: '16px' }}
+        bodyStyle={{ padding: '0 1rem' }}
+        title={(
+            <div 
+                style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    gap: '8px',
+                }}
+            >
+                <UnorderedListOutlined />
+                <Text
+                    size="sm"
+                    style={{ marginLeft: '.5rem' }}
+                >Latest Activities</Text>
+            </div>
+        )}
+    >
+        {isLoading ? (
+            <List
+                itemLayout='horizontal'
+                dataSource={Array.from({ length: 5 })
+                    .map((_, i) => ({ id: i }))}
+                renderItem={(_, index) => (
+                    <LatestActivitiesSkeleton key={index} />
+                )}
+            />
+        ) : (
+            <List />
+        )}
+    </Card>
   )
 }
 
