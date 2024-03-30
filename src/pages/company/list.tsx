682f47e9cc3676b008ae032e39dbbeb9fe7589e4
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2024-03-30 19:25:25 -0300

    column title added to the table

--- src/pages/company/list.tsx
+++ src/pages/company/list.tsx
@@ -1,8 +1,23 @@
-import { CreateButton, List } from "@refinedev/antd"
-import { useGo } from "@refinedev/core"
+import CustomAvatar from "@/components/custom-avatar";
+import { Text } from "@/components/text";
+import { COMPANIES_LIST_QUERY } from "@/graphql/queries";
+import { Company } from "@/graphql/schema.types";
+import { SearchOutlined } from "@ant-design/icons";
+import { CreateButton, FilterDropdown, List, useTable } from "@refinedev/antd"
+import { getDefaultFilter, useGo } from "@refinedev/core"
+import { Input, Space, Table } from "antd";
 
 export const CompanyList = () => {
   const go = useGo();
+  const { tableProps, filters } = useTable({
+    resource: 'companies',
+    pagination: {
+      pageSize: 28,
+    },
+    meta: {
+      gqlQuery: COMPANIES_LIST_QUERY
+    }
+  });
   return (
     <List
       breadcrumb={false}
@@ -22,6 +37,37 @@ export const CompanyList = () => {
           }}
         />
       )}
-    ></List>
+    >
+      <Table
+        {...tableProps}
+        pagination={{
+          ...tableProps.pagination,
+        }}
+      >
+        <Table.Column<Company> 
+          dataIndex="name"
+          title="Company Title"
+          defaultFilteredValue={getDefaultFilter('id', filters)}
+          filterIcon={<SearchOutlined />}
+          filterDropdown={(props) => (
+            <FilterDropdown {...props}>
+              <Input placeholder="Search Company" />
+            </FilterDropdown>
+          )}
+          render={(value, record) => (
+            <Space>
+              <CustomAvatar 
+                shape="square"
+                src={record.avatarUrl}
+                name={record.name}  
+              />
+              <Text style={{  whiteSpace: 'nowrap' }}>
+                {record.name}
+              </Text>
+            </Space>
+          )}
+        />
+      </Table>
+    </List>
   )
 }
