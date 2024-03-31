74e2863831cfdac1c357e728652b69450cd2fbec
Bruno Correia Nicolai <brunnonicolai@gmail.com>
2024-03-31 15:13:27 -0300

    create new company modal with mock users

--- src/pages/company/create.tsx
+++ src/pages/company/create.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import { useModalForm, useSelect } from '@refinedev/antd'
+import { CompanyList } from './list'
+import { Form, Input, Modal, Select } from 'antd'
+import { useGo } from '@refinedev/core'
+import { CREATE_COMPANY_MUTATION } from '@/graphql/mutations'
+import { USERS_SELECT_QUERY } from '@/graphql/queries'
+import SelectOptionWithAvatar from '@/components/select-option-with-avatar'
 
 const Create = () => {
-  return (
-    <div>Create</div>
-  )
+    const go = useGo();
+    const goToListPage = () => {
+        go({
+            to: { resource: 'companies', action: 'list' },
+            options: { keepQuery: true }, 
+            type: 'replace',
+        })
+    }
+    const { formProps, modalProps } = useModalForm({
+        action: 'create',
+        redirect: false,
+        resource: 'companies',
+        defaultVisible: true,
+        mutationMode: 'pessimistic',
+        onMutationSuccess: goToListPage,
+        meta: {
+            gqlMutation: CREATE_COMPANY_MUTATION,
+        },
+    })
+    const { selectProps, queryResult } = useSelect({
+        resource: 'users',
+        optionLabel: 'name',
+        meta: {
+            gqlQuery: USERS_SELECT_QUERY,
+        }
+    })
+    return (
+        <CompanyList>
+            <Modal
+                {...modalProps}
+                mask={true}
+                onCancel={goToListPage}
+                title="Create Company"
+                width={512}
+            >
+                <Form
+                    {...formProps}
+                    layout="vertical"
+                >
+                    <Form.Item
+                        label="Company Name"
+                        name="name"
+                        rules={[{required: true}]}
+                    >
+                        <Input placeholder="Please enter a name for the company" />
+                    </Form.Item>
+                    <Form.Item
+                        label="Responsible"
+                        name="salesOwnerId"
+                        rules={[{required: true}]}
+                    >
+                        <Select 
+                            placeholder="Please choose the process moderator" 
+                            {...selectProps}
+                        />
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </CompanyList>
+    );
 }
 
 export default Create
